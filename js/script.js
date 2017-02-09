82df91b4ffe8ca18681be2f0c9d401bfe356d3db
Collilieux Brice <collilieux.brice@gmail.com>
2017-02-09 12:56:36 +0100

    rectif de groupe

--- js/script.js
+++ js/script.js
@@ -32,8 +32,8 @@ $('ul.nav > li').click(function (e) {
 //<script>
 	$(document).ready(function() {
 		$('.js-scrollTo').on('click', function() { // Au clic sur un élément
-			var page = $(this).attr('href'); // Page cible
-			var speed = 750; // Durée de l'animation (en ms)
+			let page = $(this).attr('href'); // Page cible
+			let speed = 750; // Durée de l'animation (en ms)
 			$('html, body').animate( { scrollTop: $(page).offset().top }, speed ); // Go
 			return false;
 		});
